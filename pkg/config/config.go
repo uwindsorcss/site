5d510848adbb06f8530675741c0e054b97800b54
Ryan Prairie <me@ryanprairie.com>
2022-08-22 00:39:13 -0400

    Delete: commented old code that I forgot
    I also added the embedded version of the file to gitignore

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -45,34 +45,4 @@ func Load(paths ...string) (*koanf.Koanf, error) {
 
 	fmt.Printf("%#v\n", k.Raw())
 	return nil, nil
-
-	//// read in embedded config
-	//r := bytes.NewReader(eConfigFile)
-	//if r == nil {
-	//	fmt.Printf("bruhss\n")
-	//}
-	//err := v.ReadConfig(r)
-	//if err != nil {
-	//	return nil, fmt.Errorf("config.Load: %w", err)
-	//}
-
-	//// try to read these
-	//for _, path := range paths {
-	//	f, err := os.Open(path)
-	//	if err != nil {
-	//		if errors.Is(err, os.ErrNotExist) {
-	//			fmt.Printf("%q: not found\n", path)
-	//			continue
-	//		}
-	//		return nil, fmt.Errorf("config.Load: %q: %w", path, err)
-	//	}
-
-	//	v.MergeConfig(f)
-	//	if err != nil {
-	//		return nil, fmt.Errorf("config.Load: %w", err)
-	//	}
-	//}
-
-	//fmt.Printf("hi: %#v\n", v.AllSettings())
-	return nil, nil
 }
